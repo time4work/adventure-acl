b312092d671fb87e1d1fa171ff527340c64139f0
Nik <m.onevegas@gmail.com>
2021-10-21 18:05:58 +0200

    MQCient typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const MQCient = require('./lib/mq');
+const MQClient = require('./lib/mq');
 const MongoClient = require('./lib/mongo');
 
 console.log('[ACL] Connected');
 
-module.exports.MQCient = MQCient;
+module.exports.MQClient = MQClient;
 module.exports.MongoClient = MongoClient;
