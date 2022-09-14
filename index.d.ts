092951244bad0c3d1165bb1eb4387968dd61bd83
Nikita Lazariev <m3.onevegas@gmail.com.com>
2022-09-15 01:07:22 +0200

    export MongoModels class instead of variable

--- index.d.ts
+++ index.d.ts
@@ -1,43 +1,46 @@
 declare module 'adventure-acl' {
-    import { Connection as MQConnection } from "amqplib";
-    import { 
+    import { Connection as MQConnection } from "amqplib"
+    import {
         Connection as MongoConnection,
         ConnectOptions as MongoConnectOptions,
         Model as MongoModel
-    } from "mongoose";
+    } from "mongoose"
 
     // MQ Client
     export interface MQClientConfigInterface {
-        url: string;
-        reconnect?: boolean;
-        retryConnectionTime?: number;
+        url: string
+        reconnect?: boolean
+        retryConnectionTime?: number
     }
 
     export class MQClient {
-        constructor(config: MQClientConfigInterface);
-        public connect(): Promise<void>;
-        public get connection(): MQConnection;
+        constructor(config: MQClientConfigInterface)
+        public connect(): Promise<void>
+        public get connection(): MQConnection
     }
 
     // Mongo Client
     export interface MongoClientConfigInterface {
-        url: string;
-        reconnect?: boolean;
-        retryConnectionTime?: number;
+        url: string
+        reconnect?: boolean
+        retryConnectionTime?: number
     }
     
     export class MongoClient {
-        constructor(config: MongoClientConfigInterface, mongoOptions: MongoConnectOptions);
-        constructor(config: MongoClientConfigInterface);
-        public connect(): Promise<void>;
-        public get connection(): MongoConnection;
+        constructor(config: MongoClientConfigInterface, mongoOptions: MongoConnectOptions)
+        constructor(config: MongoClientConfigInterface)
+        public connect(): Promise<void>
+        public get connection(): MongoConnection
     }
 
     // Mongo Models
     export interface MongoModelInterface {
-        [modelName: string]: MongoModel;
+        UserModel: MongoModel
     }
 
-    export const models: MongoModelInterface;
+    export class MongoModels {
+        constructor()
+        public get models(): MongoModelInterface
+    }
 }
 
